05830eedbe42dd95aeab5cc13dcada3598516a60
Will Brinkman <willbkman@gmail.com>
2023-05-01 18:49:48 -0500

    Added a function that prints an array of drinks that cost  or less

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const drinkNames = (drink) => {
 // Print drink names
 console.log(coffeeMenu.map(drinkNames))
 
-// 3. Prui
+// 3. Print an array of drinks that cost 5 and under
+// Create function to select drinks 5 and under
+
+const drinksUnderFive = (drink) => {
+    return drink.price <= 5
+}
+
+// Use filter function to print drinks that return true with drinksUnderFive function
+console.log(JSON.stringify(coffeeMenu.filter(drinksUnderFive)))
+console.table(coffeeMenu.filter(drinksUnderFive))
+
+// 4. Print an Array of drinks that are priced at an even number
