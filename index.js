0fd6e9cf3fc880ad7715d404237e269a7d0a2d1a
Will Brinkman <willbkman@gmail.com>
2023-05-01 19:32:28 -0500

    Added a function to add text to seasonal drink names

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const drinksUnderFive = (drink) => {
 
 // Use filter function to print drinks that return true with drinksUnderFive function
 console.log(JSON.stringify(coffeeMenu.filter(drinksUnderFive)))
-console.table(coffeeMenu.filter(drinksUnderFive))
+const drinksUnderFiveArr = coffeeMenu.filter(drinksUnderFive)
+// print array of drinks under five
+console.table(drinksUnderFiveArr)
 
 // 4. Print an Array of drinks that are priced at an even number
 const EvenNumPricedDrinks = (drink) => {
@@ -27,6 +29,7 @@ const EvenNumPricedDrinks = (drink) => {
 // use filter function to return even priced drinks, assign to variable
 const evenPricedDrinks = coffeeMenu.filter(EvenNumPricedDrinks)
 
+// print even priced drinks
 console.table(evenPricedDrinks)
 
 // 5. Print the total cost if you ordered one of ever menu item
@@ -41,19 +44,37 @@ const sumTotalCost = (accum, currentVal) => {
     return accum + currentVal
 }
 
+// Initialize totalCost variable with the 
 const totalCost = drinkPricesArr.reduce(sumTotalCost);
 console.log(`Ordering one of every item will cost $${totalCost}`)
 
 // 6. Print an array with all the drinks that are seasonal.
-
+// Check if drink is seasonal, return if true
 const seasonalDrinks = (drink) => {
     return drink.seasonal
 }
 
+// filter coffeeMenu array using seasonalDrinks function
 const seasonalDrinksArr = coffeeMenu.filter(seasonalDrinks)
-
+// print modified array
 console.table(seasonalDrinksArr)
 
 // 7. Print all the seasonal drinks with the words "with imported beans" after the item name.
 // For example: "affogato with imported beans".
+// Build function to create a string with drink.name and the requested text
 
+// const seasonalDrinkNames = (drink) => {
+//    return drink.name +=  "with imported beans"
+// }
+//const seasonalDrinkNames = seasonalDrinksArr.map(drink => {
+    //     return `${drink.name} with imported beans`;
+    // });
+
+// instead of modifying original coffeeMenu array with .forEach
+const modifiedSeasonalDrinks = (drink) => {
+    return {
+        ...drink, // spread the original object to create a copy
+        name: `${drink.name} with imported beans` 
+    };
+};
+console.table(seasonalDrinksArr.map(modifiedSeasonalDrinks))
